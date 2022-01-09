1ea9fde36391d0ad416589c9a3fc11f53ef635a7
dna2121 <dnatayawibisono@gmail.com>
2022-01-09 22:07:25 +0700

    commit

---
+++ ClientChatApp_082_DeniseNW/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using WCFService_2Way_082;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientChatApp_082_DeniseNW
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            InstanceContext context = new InstanceContext(new ClientCallback());
+            ServiceReference1.ServiceCallbackClient server = new ServiceReference1.ServiceCallbackClient(context);
+
+            Console.WriteLine("Masukkan Username : ");
+            string nama = Console.ReadLine();
+            server.gabung(nama);
+
+            Console.WriteLine("Kirim Pesan : ");
+            string pesan = Console.ReadLine();
+
+            while (true)
+            {
+                if (!string.IsNullOrEmpty(pesan))
+                    server.kirimPesan(pesan);
+                Console.WriteLine("Kirim Pesan : " + "\n");
+                pesan = Console.ReadLine();
+            }
+        }
+    }
+
+    public class ClientCallback : ServiceReference1.IServiceCallbackCallback
+    {
+        public void pesanKirim(string user, string pesan)
+        {
+            Console.WriteLine("{0}: {1}", user, pesan);
+        }
+
+    }
+}
---
+++ ClientChatApp_082_DeniseNW/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClientChatApp_082_DeniseNW")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ClientChatApp_082_DeniseNW")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a3c886b3-d76d-4a3d-ae50-1b06ebf7358c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
